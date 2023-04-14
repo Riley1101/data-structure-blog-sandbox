ac69e36b865b2cf010bef4a7686bce7d9f7422d7
Riley1101 <www.riley.s.eileen@gmail.com>
2023-04-13 22:44:59 -0400

    Add implementation of depth-first tree traversals in new file

---
+++ src/tree/dft_traversals.ts
@@ -0,0 +1,51 @@
+class Node {
+    value : number;
+    left : Node | null;
+    right : Node | null;
+    constructor(value:number){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree{
+    root : Node;
+    constructor(){
+        this.root = null;
+    }
+    push(value : number) {
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode 
+            return newNode
+        }
+        let tmp = this.root
+        while(true){
+            if(value <  tmp.value){
+                if(!tmp.left)  {
+                    tmp.left = newNode
+                    return newNode
+                }else{
+                    tmp = tmp.left
+                }
+            }else{
+                if(!tmp.right)  {
+                    tmp.right= newNode
+                    return newNode
+                }else{
+                    tmp = tmp.right
+                }
+            }
+        }
+    }
+}
+
+const t1 = new Tree()
+t1.push(1)
+t1.push(3)
+t1.push(2)
+t1.push(7)
+t1.push(9)
+t1.push(12)
+export {}
