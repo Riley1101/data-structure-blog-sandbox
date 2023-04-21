dfffc020c14713d3278a3431061de0ed12afec8d
Riley1101 <www.riley.s.eileen@gmail.com>
2023-04-21 10:33:37 -0400

    Create a binary search tree class with Node and Tree classes, and implement pre-order, in-order, and post-order traversals

---
+++ src/sandbox/day_12.ts
@@ -0,0 +1,79 @@
+class Node {
+    value : number;
+    left : Node | null;
+    right : Node |  null;
+    constructor(value: number){
+        this.value = value
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree{
+    root : Node
+    constructor(){
+        this.root = null;
+    }
+
+    push(value : number){
+        let newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return newNode; 
+        }
+        let tmp = this.root
+        while(true) {
+            if(value < tmp.value){
+                if(tmp.left === null){
+                    tmp.left = newNode 
+                    return newNode 
+                }
+                tmp = tmp.left
+            }else{
+                if(tmp.right=== null){
+                    tmp.right= newNode 
+                    return newNode 
+                }
+                tmp = tmp.right
+            }
+        }
+    }
+}
+
+let tr = new Tree()
+tr.push(1)
+tr.push(3)
+tr.push(2)
+tr.push(4)
+tr.push(6)
+tr.push(5)
+console.log(tr.root)
+
+const preOrderTraveral = (node : Node, arr : number[])=>{
+    if(!node) return arr;
+    arr.push(node.value)
+    arr = preOrderTraveral(node.left,arr)
+    arr = preOrderTraveral(node.right,arr)
+    return arr
+}
+console.log(preOrderTraveral(tr.root,[]))
+
+const inOrderTraversal= (node : Node, arr : number[])=>{
+    if(!node) return arr;
+    arr = inOrderTraversal(node.left,arr)
+    arr = inOrderTraversal(node.right,arr)
+    arr.push(node.value)
+    return arr
+}
+console.log(inOrderTraversal(tr.root,[]))
+
+const postOrder= (node : Node, arr : number[])=>{
+    if(!node) return arr;
+    arr = postOrder(node.left,arr)
+    arr = postOrder(node.right,arr)
+    arr.push(node.value)
+    return arr
+}
+console.log(postOrder(tr.root,[]))
+
+export {}
