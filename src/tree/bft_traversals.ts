18f0221ef931ec7242a00a6c84dd1009d3950fbe
Riley1101 <www.riley.s.eileen@gmail.com>
2023-04-23 13:49:26 -0400

    Refactor breadthFirstTraversals functions to use iterative and recursive approaches, and add tests

--- src/tree/bft_traversals.ts
+++ src/tree/bft_traversals.ts
@@ -43,11 +43,34 @@ class Tree{
 }
 
 const t1 = new Tree()
-t1.push(1)
+t1.push(8)
 t1.push(3)
-t1.push(2)
-t1.push(7)
-t1.push(9)
-t1.push(12)
+t1.push(10)
+t1.push(1)
+t1.push(6)
+t1.push(15)
+
+console.log(t1.root)
+
+function breadthFirstTraversals( queue: Node[], arr: number[]){
+    if(!queue.length) return arr;
+    let node = queue.shift()
+    arr.push(node.value)
+    if(node.left) queue.push(node.left)
+    if(node.right) queue.push(node.right)
+    return breadthFirstTraversals(queue,arr)
+}
+console.log(breadthFirstTraversals([t1.root], []))
+
+function breadthFirstTraversals2( queue: Node[], arr: number[]){
+    while(queue.length){
+        const node = queue.shift()
+        arr.push(node.value)
+        if(node.left) queue.push(node.left)
+        if(node.right) queue.push(node.right)
+    }
+    return arr
+}
+console.log(breadthFirstTraversals2([t1.root], []))
 
 export {}
