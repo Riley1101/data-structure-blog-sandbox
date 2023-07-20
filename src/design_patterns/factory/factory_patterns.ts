3cd02ffdff9e05bdd35a871ec976311c6e55eddd
Riley1101 <arkardev00@gmail.com>
2023-07-20 14:32:55 -0400

    added factory_patterns

---
+++ src/design_patterns/factory/factory_patterns.ts
@@ -0,0 +1,75 @@
+export interface BoreingPlane extends Plane {
+  fly(): string;
+  land(): string;
+  getName(): string;
+}
+
+export class BoreingPlane implements Plane {
+  private _name: string;
+  constructor() {
+    this._name = "boreing";
+  }
+  fly(): string {
+    return `I am ${this._name} and I am flying boreingly`;
+  }
+  land(): string {
+    return `I am ${this._name} and I am landing boreingly`;
+  }
+}
+export interface AirPlane extends Plane {
+  fly(): string;
+  land(): string;
+  getName(): string;
+}
+
+export class AirPlane implements AirPlane {
+  private _name: string;
+  constructor() {
+    this._name = "airplane";
+  }
+  fly(): string {
+    return `I am ${this._name} and I am flying airplane`;
+  }
+  land(): string {
+    return `I am ${this._name} and I am landing airplane`;
+  }
+}
+export interface Plane {
+  fly(): string;
+  land(): string;
+  getName(): string;
+}
+
+export class Plane implements Plane {
+  private name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  fly(): string {
+    return `I am ${this.name} and I am flying like a plane`;
+  }
+  land(): string {
+    return `I am ${this.name} and I am landing like a plane`;
+  }
+}
+
+export class PlaneFactory {
+  static createPlane(type: string): Plane {
+    switch (type) {
+      case "air": {
+        return new AirPlane();
+      }
+      case "boreing": {
+        return new BoreingPlane();
+      }
+    }
+  }
+}
+
+let airplane = PlaneFactory.createPlane("air");
+console.log(airplane.fly());
+console.log(airplane.land());
+
+let boreing = PlaneFactory.createPlane("boreing");
+console.log(boreing.fly());
+console.log(boreing.land());
